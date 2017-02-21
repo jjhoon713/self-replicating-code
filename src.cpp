8a558b2a903144c44887f46221ceac276af62fec
jjhoon713 <ingyeok@gmail.com>
2017-02-21 15:29:54 +0800

    Create src.cpp

---
+++ src.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+string a = R"(int main(int argc, char *argv[]) {
+	if (argc == 2) {
+		ofstream myfile;
+		myfile.open(argv[1]);
+		myfile << "#include <iostream>\n#include <string>\n#include <fstream>\nusing namespace std;\nstring a = R" << (char)34 << (char)40 << a << (char)41 << (char)34 << (char)59 << endl << a;
+		myfile.close();
+		return 0;
+	}
+	return 0;
+}
+//Jihoon Chung 20311792)";
+int main(int argc, char *argv[]) {
+	if (argc == 2) {
+		ofstream myfile;
+		myfile.open(argv[1]);
+		myfile << "#include <iostream>\n#include <string>\n#include <fstream>\nusing namespace std;\nstring a = R" << (char)34 << (char)40 << a << (char)41 << (char)34 << (char)59 << endl << a;
+		myfile.close();
+		return 0;
+	}
+	return 0;
+}
+//Jihoon Chung 20311792
